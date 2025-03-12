84ce31ff429b9073bb209d8c524aa9ecf52c3c1e
Dodong2 <carlseighartaliode@gmail.com>
2025-03-12 13:27:54 +0800

    deployment test 2

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGOURL)
+        await mongoose.connect(process.env.MONGOURL, {
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10,
+        })
         console.log(`Connected to the Database!`)
     } catch(err) {
         console.log('Failed to connect to the Database! : ', err)
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB  
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://testinventory.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }))
